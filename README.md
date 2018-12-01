c5979c261a331942623c0635307fde043b0eacb6
Jeff Lee <jeffomatic@gmail.com>
2018-12-01 12:43:27 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# SublimeFmt
+
+Sublime Text 3 plugin for running code formatters such as `gofmt`, `ocp-indent`, etc. on save.
+
+## Prereqs
+
+If you're running on a Mac, you'll want to install the [FixMacPath](https://github.com/int3h/SublimeFixMacPath) plugin, so that other plugins have access to your `$PATH`.
+
+## Configuration options
+
+Configuration is available under Preferences > Package Settings > SublimeFmt.
+
+You'll want to create formatter entries under the `formatters` object:
+
+```
+{
+  "formatters": [
+    {
+      "cmd": "ocp-indent",
+      "extensions": [".ml"]
+    }
+  ]
+}
+```
+
+A formatter configuration has two required parameters:
+
+Name | Type | Description
+--- | --- | ---
+`cmd` | string | Absolute path to formatting command. SublimeFmt assumes that the command takes an input filename as its last argument, and emits the result to stdout.
+`extensions` | array<string> | The plugin will only run for files with extensions specified here.
+
+You can also further restrict where the formatter runs by using exclude and include paths:
+
+Name | Type | Description
+--- | --- | ---
+`folder_exclude_path` | array<string> | The plugin will ignore files whose paths contain patterns in this setting. Defaults to `[]` (no paths will be excluded).
+`folder_include_path` | array<string> | If this setting is non-empty, the plugin will only run for files whose paths are specified in this setting. Defaults to `[]` (all paths will be accepted, except those excluded by `folder_exclude_path`).
+
+SublimeFmt will run the first formatter whose settings match the path of the file being saved.
---
+++ SublimeFmt.py
@@ -0,0 +1,114 @@
+import io
+import os
+import sublime
+import sublime_plugin
+import subprocess
+import tempfile
+import time
+
+
+def path_contains_pattern(file_path, pattern):
+    """Return True if file_path contains pattern anywhere except the filename itself."""
+    path = os.path.dirname(os.path.normpath(file_path))
+    pattern = os.path.normpath(pattern)
+
+    pattern_toks = pattern.split(os.sep)
+    if len(pattern_toks) == 0:
+        return False
+
+    pat_i = 0
+    for path_tok in path.split(os.sep):
+        if path_tok != pattern_toks[pat_i]:
+            pat_i = 0
+            continue
+
+        if pat_i == len(pattern_toks) - 1:
+            return True
+
+        pat_i += 1
+
+    return False
+
+
+def find_fmt_command(src_path):
+    formatters = sublime.load_settings('SublimeFmt.sublime-settings').get(
+        'formatters', [])
+    for config in formatters:
+        if path_matches_formatter(src_path, config):
+            cmd = config['cmd']
+            if not isinstance(cmd, str):
+                raise TypeError(
+                    'cmd for formatter matching "%s" is not a string' %
+                    (src_path))
+            return config['cmd']
+
+    return None
+
+
+def path_matches_formatter(src_path, config):
+    """Return True if the given formatter matches the provided path"""
+
+    # Extension filter
+    if 'extensions' not in config:
+        return False
+
+    _, ext = os.path.splitext(src_path)
+    if ext not in config['extensions']:
+        return False
+
+    # Path inclusion filter
+    include_patterns = config.get('folder_include_patterns', [])
+    if len(include_patterns) > 0:
+        if not any(
+                path_contains_pattern(src_path, pattern)
+                for pattern in include_patterns):
+            return False
+
+    # Path exclusion filter
+    exclude_patterns = config.get('folder_exclude_patterns', [])
+    if any(
+            path_contains_pattern(src_path, pattern)
+            for pattern in exclude_patterns):
+        return False
+
+    return True
+
+
+class SublimeFmtCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        src_path = self.view.file_name()
+        fmt_cmd = find_fmt_command(src_path)
+        if fmt_cmd is None:
+            return
+
+        start = time.time()
+
+        region = sublime.Region(0, self.view.size())
+        contents = self.view.substr(region)
+
+        # Copy the view buffer into a tempfile, pass the contents to the formatter
+        # command.
+        fd, p = tempfile.mkstemp()
+        try:
+            os.write(fd, bytes(contents, 'UTF-8'))
+            os.close(fd)
+
+            cmd = '%s "%s"' % (fmt_cmd, p)
+            formatted = subprocess.check_output(
+                cmd, shell=True).decode('UTF-8')
+            self.view.replace(edit, region, formatted)
+        finally:
+            # Ensure that the tempile fd is closed, and that the tempfile is removed.
+            try:
+                os.close(fd)
+            except OSError:
+                pass
+
+            os.unlink(p)
+
+        print('SublimeFmt executed in %.3fs' % (time.time() - start))
+
+
+class SublimeFmtListener(sublime_plugin.EventListener):
+    def on_pre_save(self, view):
+        view.run_command('sublime_fmt')
